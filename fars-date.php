023aed40196060bbf72c22ec15d648d78693532b
Mohammad Salehi Koleti <mds1121@yahoo.com>
2020-07-10 20:18:08 +0430

    start entry

---
+++ fars-date.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Classic Editor
+ *
+ * Plugin Name: Fars Date
+ * Plugin URI:  https://wordpress.org/plugins/fars-date/
+ * Description: Adds Suppport for Jalali (Shamsi) date and Persian numbers to posts, comments, pages, archives, search, categories, permalinks; to have better experience with Persian sites 
+ * Version:     1.0
+ * Author:      Mohammad Salehi Koleti
+ * Author URI:  http://Karket.ir
+ * License:     GPLv3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: fars-date
+ *
+ * Fars Date is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+ */
+if (!defined('ABSPATH')) exit('No direct access allowed');
+
+
+ ?>
